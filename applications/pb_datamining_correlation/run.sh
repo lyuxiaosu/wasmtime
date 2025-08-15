55677ae486aa00feec93887e19d75e8bc56e2004
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2025-08-15 10:52:10 -0600

    update run.sh, upload common.mk

--- applications/pb_datamining_correlation/run.sh
+++ applications/pb_datamining_correlation/run.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+set -e
 
-# 存储所有执行时间
 times=()
 
-# 执行 1000 次
-for i in {1..1000}; do
-    # 捕获输出，提取时间（假设输出格式是 "Execution time: <num> ns"）
+for i in {1..10000}; do
     t=$(./pb_datamining_correlation.native | awk '{print $3}')
-    times+=($t)
+    t_us=$((t / 1000))
+    times+=($t_us)
 done
 
-# 排序并取中位数
 sorted=($(printf '%s\n' "${times[@]}" | sort -n))
 len=${#sorted[@]}
+
 if (( len % 2 == 1 )); then
-    # 奇数个元素，中位数是中间值
     median=${sorted[$((len/2))]}
 else
-    # 偶数个元素，中位数是中间两个的平均
     mid=$((len/2))
     median=$(( (sorted[mid-1] + sorted[mid]) / 2 ))
 fi
 
-echo "Median execution time (ns): $median"
+min=${sorted[0]}
+max=${sorted[-1]}
+
+echo "Min execution time (us): $min"
+echo "Max execution time (us): $max"
+echo "Median execution time (us): $median"
--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+set -e
 
 times=()
 
 for i in {1..1000}; do
+    # 取第三列数字（ns），再转成 us
     t=$(./wasmtime-my_test | awk '{print $3}')
-    times+=($t)
+    t_us=$((t / 1000))
+    times+=($t_us)
 done
 
+# 排序
 sorted=($(printf '%s\n' "${times[@]}" | sort -n))
 len=${#sorted[@]}
+
+# 中位数
 if (( len % 2 == 1 )); then
     median=${sorted[$((len/2))]}
 else
@@ -16,4 +22,10 @@ else
     median=$(( (sorted[mid-1] + sorted[mid]) / 2 ))
 fi
 
-echo "Median execution time (ns): $median"
+# 最小值 & 最大值
+min=${sorted[0]}
+max=${sorted[-1]}
+
+echo "Min execution time (us): $min"
+echo "Max execution time (us): $max"
+echo "Median execution time (us): $median"
